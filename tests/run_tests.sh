ab031057169a519c3ffc97f560189df365be70b5
Stepan Blyschak <stepanb@nvidia.com>
2020-10-23 14:03:35 +0300

    Revert "[multi_passwd_ssh] Enable for group sonic (#2129)"
    This reverts commit a546590ecad39f9c3fa9f4279c84b6a640d3035e.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -84,7 +84,6 @@ function setup_environment()
 
     export ANSIBLE_CONFIG=${BASE_PATH}/ansible
     export ANSIBLE_LIBRARY=${BASE_PATH}/ansible/library/
-    export ANSIBLE_CONNECTION_PLUGINS=${BASE_PATH}/ansible/plugins/connection
 }
 
 function setup_test_options()
