f3ee1a67a53d303070cf7241f1bba249195b99f5
Noa Or <noaor@nvidia.com>
2020-10-29 15:28:15 +0200

    Add L1 Aggregated test cases
    Change-Id: Iac5cd20bb8d302c9f2f796cfe098be958039a598

--- tests/drop_packets/test_wjh.py
+++ tests/drop_packets/test_wjh.py
@@ -7,6 +7,9 @@ from drop_packets import *
 from ptf.testutils import verify_no_packet_any, simple_ip_only_packet, simple_ipv4ip_packet
 from tests.common.helpers.assertions import pytest_assert
 from collections import OrderedDict
+from tests.common.platform.device_utils import fanout_switch_port_lookup
+from tests.common.utilities import wait
+import json
 
 
 logger = logging.getLogger(__name__)
@@ -91,8 +94,8 @@ def get_raw_table_output(duthost, command="show what-just-happened"):
     return table_output
 
 
-def get_agg_table_output(duthost):
-    stdout = duthost.command("show what-just-happened poll --aggregate")
+def get_agg_table_output(duthost, command="show what-just-happened --aggregate"):
+    stdout = duthost.command(command)
     if stdout['rc'] != 0:
         raise Exception(stdout['stdout'] + stdout['stderr'])
     splitted_table = stdout['stdout'].splitlines()[3:]
@@ -215,18 +218,6 @@ def verify_l1_raw_drop_exists(table, port):
     return entry_exists
 
 
-def do_l1_raw_test(duthost, port):
-    # shut down one of the ports
-    duthost.command("config interface shutdown {}".format(port))
-
-    try:
-        table = get_raw_table_output(duthost, "show what-just-happened l1")
-        if not verify_l1_raw_drop_exists(table, port):
-            pytest.fail("Could not find L1 drop on WJH table.")
-    finally:
-        duthost.command("config interface startup {}".format(port))
-
-
 @pytest.fixture(scope='module', autouse=True)
 def check_global_configuration(duthost):
     global_conf = {}
@@ -242,29 +233,10 @@ def check_global_configuration(duthost):
         pytest.skip("Debug mode is not enabled. Skipping test.")
 
 
-def channel_conf(channel):
-    channel_conf = {}
-    channels_iter = iter(range(len(channel)))
-    for i in channels_iter:
-        channel_conf[channel[i]] = channel[i+1]
-        next(channels_iter, None)
-
-    return channel_conf
-
 @pytest.fixture(scope='module', autouse=True)
 def get_channel_configuration(duthost):
-    channels = {}
-
-    forwarding = duthost.shell('sonic-db-cli CONFIG_DB hgetall "WJH_CHANNEL|forwarding"', module_ignore_errors=False)['stdout_lines']
-    pytest_assert(forwarding is not None, "WJH_CHANNEL|forwarding does not exist in config_db")
-
-    l1 = duthost.shell('sonic-db-cli CONFIG_DB hgetall "WJH_CHANNEL|l1"', module_ignore_errors=False)['stdout_lines']
-    pytest_assert(l1 is not None, "WJH_CHANNEL|l1 does not exist in config_db")
-
-    channels['forwarding'] = channel_conf(forwarding)
-    channels['l1'] = channel_conf(l1)
-
-    pytest.CHANNEL_CONF = channels
+    config_facts  = duthost.config_facts(host=duthost.hostname, source="running")['ansible_facts']
+    pytest.CHANNEL_CONF = config_facts.get('WJH_CHANNEL', {})
 
 
 @pytest.fixture(scope='module', autouse=True)
@@ -272,53 +244,6 @@ def check_feature_enabled(duthost):
     features = duthost.feature_facts()['ansible_facts']['feature_facts']
     if 'what-just-happened' not in features or features['what-just-happened'] != 'enabled':
         pytest.skip("what-just-happened feature is not available. Skipping the test.")
-
-
-def get_interfaces_status(duthost):
-    stdout = duthost.command("show interfaces status")
-    if stdout['rc'] != 0:
-        raise Exception(stdout['stdout'] + stdout['stderr'])
-
-    interfaces = stdout['stdout']
-
-    data = OrderedDict()
-    headers = []
-    header_lines_num = 2
-    sep_index = 1
-    table_lines = interfaces.splitlines()
-    if not table_lines:
-        return None
-
-    separators = re.split(r'\s{2,}', interfaces.splitlines()[sep_index])  # separators between headers and content
-    headers_line = table_lines[0]
-    start = 0
-    # separate headers by table separators
-    for sep in separators:
-        curr_len = len(sep)
-        headers.append(headers_line[start:start+curr_len].strip())
-        start += curr_len + 2
-
-    interface_index = 0
-    admin_index = 8
-
-    for i in range(len(headers)):
-        if headers[i] == "Interface":
-            interface_index = i
-        if headers[i] == "Admin":
-            admin_index = i
-
-    interface_sep = len(separators[interface_index])
-    admin_start = 0
-    for j in range(admin_index):
-        admin_start += len(separators[j]) + 2
-    admin_end = admin_start + len(separators[admin_index])
-
-    output_lines = interfaces.splitlines()[header_lines_num:]  # Skip the header lines in output
-
-    for line in output_lines:
-        data[line[0:interface_sep].strip()] = line[admin_start:admin_end].strip()
-
-    return data
 
 
 def test_tunnel_ip_in_ip(do_test, ptfadapter, duthost, setup, pkt_fields, ports_info):
@@ -368,15 +293,101 @@ def test_tunnel_ip_in_ip(do_test, ptfadapter, duthost, setup, pkt_fields, ports_
     do_test("L3", pkt, ptfadapter, duthost, ports_info, setup['neighbor_sniff_ports'])
 
 
-def test_l1_raw_drop(do_test, duthost):
+def check_if_l1_enabled(type):
     if "l1" not in pytest.CHANNEL_CONF:
         pytest.skip("L1 channel is not confiugred on WJH.")
-    if pytest.CHANNEL_CONF['l1']['type'].find('raw') == -1:
+    if pytest.CHANNEL_CONF['l1']['type'].find(type) == -1:
         pytest.skip("L1 raw channel type is not confiugred on WJH.")
 
-    interfaces = get_interfaces_status(duthost)
-    for port in interfaces.keys():
-        if interfaces[port] == 'up':
+
+def get_active_port(duthost):
+    intf_facts = duthost.interface_facts()['ansible_facts']['ansible_interface_facts']
+
+    for port in intf_facts.keys():
+        if intf_facts[port]['active'] and port.startswith('Ethernet'):
             break
 
-    do_l1_raw_test(duthost, port)
+    if not intf_facts[port]['active']:
+        pytest.skip("Could not find port in active state. Skipping the test.")
+    else:
+        return port
+
+
+def test_l1_raw_drop(duthost):
+    check_if_l1_enabled('raw')
+
+    port = get_active_port(duthost)
+    # shut down one of the ports
+    duthost.command("config interface shutdown {}".format(port))
+
+    try:
+        table = get_raw_table_output(duthost, "show what-just-happened l1")
+        if not verify_l1_raw_drop_exists(table, port):
+            pytest.fail("Could not find L1 drop on WJH table.")
+    finally:
+        duthost.command("config interface startup {}".format(port))
+
+
+def verify_l1_agg_drop_exists(table, port, state):
+    entry_exists = False
+    for entry in table:
+        if (entry['State'] == state and
+            #entry['Port'] == port and TODO: uncomment when sdk bug merged
+            entry['State Change'] > 0):
+                entry_exists = True
+                break
+    if not entry_exists:
+        pytest.fail("Could not find L1 drop on WJH aggregated table.")
+    return entry
+
+
+def test_l1_agg_port_up(duthost):
+    check_if_l1_enabled('aggregate')
+
+    intf_facts = duthost.interface_facts()['ansible_facts']['ansible_interface_facts']
+
+    for port in intf_facts.keys():
+        if (not intf_facts[port]['active'] and port.startswith('Ethernet')):
+            break
+
+    if intf_facts[port]['active']:
+        pytest.skip("Could not find port in Down state. Skipping the test.")
+
+    duthost.command("config interface startup {}".format(port))
+    try:
+        table = get_agg_table_output(duthost, command="show what-just-happened l1 --aggregate")
+        entry = verify_l1_agg_drop_exists(table, port, 'Up')
+        if entry['Down Reason - Recommended Action'] != 'N/A':
+            pytest.fail("Could not find L1 drop on WJH aggregated table.")
+    finally:
+        duthost.command("config interface shutdown {}".format(port))
+
+
+def test_l1_agg_port_down(duthost):
+    check_if_l1_enabled('aggregate')
+    port = get_active_port(duthost)
+
+    duthost.command("config interface shutdown {}".format(port))
+    try:
+        table = get_agg_table_output(duthost, command="show what-just-happened l1 --aggregate")
+        entry = verify_l1_agg_drop_exists(table, port, 'Down')
+        if entry['Down Reason - Recommended Action'] != 'Port admin down - Validate port configuration':
+            pytest.fail("Could not find L1 drop on WJH aggregated table.")
+    finally:
+        duthost.command("config interface startup {}".format(port))
+
+
+def test_l1_agg_fanout_port_down(duthost, fanouthosts):
+    check_if_l1_enabled('aggregate')
+    port = get_active_port(duthost)
+
+    fanout, fanout_port = fanout_switch_port_lookup(fanouthosts, port)
+    fanout.shutdown(fanout_port)
+    wait(15, 'Wait for fanout port to shutdown')
+    try:
+        table = get_agg_table_output(duthost, command="show what-just-happened l1 --aggregate")
+        entry = verify_l1_agg_drop_exists(table, port, 'Down')
+        if entry['Down Reason - Recommended Action'] != 'Auto-negotiation failure - Set port speed manually, disable auto-negotiation':
+            pytest.fail("Could not find L1 drop on WJH aggregated table.")
+    finally:
+        fanout.no_shutdown(fanout_port)
