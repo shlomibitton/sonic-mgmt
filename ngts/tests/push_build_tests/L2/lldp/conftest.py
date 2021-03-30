e7e1d8205a310521c97e79d63b74690e2038df0f
Anton <antonh@nvidia.com>
2021-03-30 19:51:13 +0300

    fix issue with advertising via LLDP the same mac addresses on host interfaces
    Change-Id: If3df54257011384edb9fbbcf03542a968878ca16
    Signed-off-by: Anton <antonh@nvidia.com>

--- ngts/tests/push_build_tests/L2/lldp/conftest.py
+++ ngts/tests/push_build_tests/L2/lldp/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import logging
+from ngts.cli_wrappers.linux.linux_interface_clis import LinuxInterfaceCli
 
 logger = logging.getLogger()
 
@@ -16,6 +17,11 @@ def lldp_configuration(topology_obj):
         cli_object = topology_obj.players[host_alias]['cli']
         if not cli_object.lldp.is_lldp_enabled_on_host(host_engine):
             cli_object.lldp.enable_lldp_on_host(host_engine)
+            # to prevent advertising the same mac on an interfaces,
+            # need to restart ports status after lldp enbling
+            for port in topology_obj.players_all_ports[host_alias]:
+                LinuxInterfaceCli.disable_interface(host_engine, port)
+                LinuxInterfaceCli.enable_interface(host_engine, port)
     yield
 
 
