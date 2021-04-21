e009e8334f3ba83e2eb6228eaef3bd611b6765e8
Petro Pikh <petrop@nvidia.com>
2021-04-21 12:06:37 +0300

    Added logic for check data and control plane traffic loss during different reboot types
    Change-Id: Ia26fe22e2cd63b610139fa0b4009cd854e059ef4

--- ngts/tests/push_build_tests/system/test_reboot_reload.py
+++ ngts/tests/push_build_tests/system/test_reboot_reload.py
@@ -1,37 +1,32 @@
 import allure
 import logging
 import pytest
-import time
-import os
-import yaml
 
-from ngts.tools.skip_test.skip import ngts_skip
-from ngts.tools.mysql_api.mysql_api import DB
+from infra.tools.validations.traffic_validations.ping.ping_runner import PingChecker
 
 logger = logging.getLogger()
 
-reboot_types = ['fast-reboot', 'warm-reboot', 'reboot']
+validation_types = ['fast-reboot', 'warm-reboot', 'reboot', 'config reload -y']
+
+expected_traffic_loss_dict = {'fast-reboot': {'data': 30, 'control': 90},
+                              'warm-reboot': {'data': 0, 'control': 90},
+                              'reboot': {'data': 180, 'control': 180},
+                              'config reload -y': {'data': 180, 'control': 180}
+                              }
 
 
 class TestRebootReload:
 
     @pytest.fixture(autouse=True)
-    def setup(self, testdir, topology_obj, engines, platform_params, sonic_version):
+    def setup(self, topology_obj, interfaces, engines):
         self.topology_obj = topology_obj
         self.dut_engine = engines.dut
         self.cli_object = self.topology_obj.players['dut']['cli']
-
-        self.platform_params = platform_params
-        platform_index = 1
-        self.platform = self.platform_params.hwsku.split('-')[platform_index]
-        self.current_test_folder = testdir.request.fspath.dirname
-        expected_reboot_reload_time_file_path = os.path.join(self.current_test_folder,
-                                                             'expected_reboot_reload_time.yaml')
-        with open(expected_reboot_reload_time_file_path) as raw_reboot_reload_time_data:
-            expected_reboot_reload_time_dict = yaml.load(raw_reboot_reload_time_data, Loader=yaml.FullLoader)
-        self.expected_reboot_reload_time_dict = expected_reboot_reload_time_dict[self.platform]
-        self.setup_name = platform_params.setup_name
-        self.sonic_ver = sonic_version
+        self.interfaces = interfaces
+        self.ping_sender_iface = '{}.40'.format(self.interfaces.ha_dut_2)
+        self.dut_vlan40_int_ip = '40.0.0.1'
+        self.dut_port_channel_ip = '30.0.0.1'
+        self.hb_vlan40_ip = '40.0.0.3'
 
     @pytest.fixture(autouse=True)
     def ignore_expected_loganalyzer_exceptions(self, loganalyzer):
@@ -47,79 +42,84 @@ class TestRebootReload:
             loganalyzer.ignore_regex.extend(ignore_regex_list)
 
     @pytest.mark.ngts_skip({'platform_prefix_list': ['simx'], 'rm_ticket_list': [2566883]})
-    @pytest.mark.parametrize('reboot_type', reboot_types)
-    def test_push_gate_reboot(self, platform_params, request, reboot_type):
+    @pytest.mark.parametrize('validation_type', validation_types)
+    def test_push_gate_reboot(self, validation_type):
         """
         This tests checks reboot time, it doing reboot according to test parameter and after execution - it upload
         test results to MySQL DB
-        :param platform_params: pytest fixture with platform params
-        :param request: pytest buildin
-        :param reboot_type: reboot type - which will be executed
+        :param validation_type: validation type - which will be executed
         """
-        if reboot_type == 'fast-reboot':
-            ngts_skip(platform_params.platform,
-                      github_ticket_list=['https://github.com/Azure/sonic-buildimage/issues/4793'])
 
-        start_time = time.time()
-        reboot_time = 0.0
+        allowed_data_loss_time = expected_traffic_loss_dict[validation_type]['data']
+        allowed_control_loss_time = expected_traffic_loss_dict[validation_type]['control']
+
         try:
-            with allure.step('Rebooting the dut using reboot cmd: "sudo {}"'.format(reboot_type)):
-                dut_cli = self.topology_obj.players['dut']['cli']
-                dut_cli.general.reboot_flow(self.dut_engine, reboot_type=reboot_type, topology_obj=self.topology_obj,
-                                            wait_after_ping=0)
+            # Step below required, if no ARP for 30.0.0.2 warm/fast reboot will not work
+            with allure.step('Resolve ARP on DUT for IP 30.0.0.2'):
+                self.resolve_arp_static_route()
 
-            reboot_time = time.time() - start_time
-            logger.info('Test reboot: {} run time is: {}'.format(reboot_type, reboot_time))
-            expected_reboot_time = self.expected_reboot_reload_time_dict[reboot_type]
-            assert reboot_time < expected_reboot_time, 'Reboot time: {} is bigger ' \
-                                                       'than expected: {}'.format(reboot_time, expected_reboot_time)
+            with allure.step('Starting background validation for control plane traffic'):
+                control_plane_checker = self.start_control_plane_validation(validation_type, allowed_control_loss_time)
+
+            with allure.step('Starting background validation for data plane traffic'):
+                data_plane_checker = self.start_data_plane_validation(validation_type, allowed_data_loss_time)
+
+            self.do_reboot_or_reload_action(action=validation_type)
+
+            with allure.step('Checking control plane traffic loss'):
+                logger.info('Checking that control plane traffic loss not more '
+                            'than: {}'.format(allowed_control_loss_time))
+                control_plane_checker.complete_validation()
+
+            with allure.step('Checking data plane traffic loss'):
+                logger.info('Checking that data plane traffic loss not more than: {}'.format(allowed_data_loss_time))
+                data_plane_checker.complete_validation()
 
         except Exception as err:
             raise AssertionError(err)
-        finally:
-            mysql_columns_values = {
-                                    "test_name": request.node.originalname,
-                                    "setup_name": self.setup_name,
-                                    "time_stamp": time.time(),
-                                    "os_version": self.sonic_ver,
-                                    "platform": self.platform,
-                                    "reboot_type": reboot_type,
-                                    "reboot_time": reboot_time
-            }
-            logger.info('Uploading test results to MySQL DB')
-            DB().insert(table='reboot_time', columns_values=mysql_columns_values)
 
-    @pytest.mark.ngts_skip({'platform_prefix_list': ['simx'], 'rm_ticket_list': [2615940]})
-    def test_push_gate_config_reload(self, request):
-        """
-        This tests checks config reload time, it doing reload and after execution - it upload test results to MySQL DB
-        :param request: pytest buildin
-        """
-        start_time = time.time()
-        reload_time = 0.0
-        try:
-            with allure.step('Reloading the DUT config by cmd: "config reload -y"'):
+    def resolve_arp_static_route(self):
+        validation = {'sender': 'ha', 'args': {'iface': 'bond0', 'count': 3, 'dst': self.dut_port_channel_ip}}
+        ping = PingChecker(self.topology_obj.players, validation)
+        logger.info('Sending 3 ping packets to {}'.format(self.dut_port_channel_ip))
+        ping.run_validation()
+
+    def start_control_plane_validation(self, validation_type, allowed_control_loss_time):
+        validation_control_plane = {'sender': 'ha',
+                                    'name': 'control_plane_{}'.format(validation_type),
+                                    'background': 'start',
+                                    'args': {'interface': self.ping_sender_iface,
+                                             'count': 200, 'dst': self.dut_vlan40_int_ip,
+                                             'allowed_traffic_interruptions': 1,
+                                             'allowed_traffic_loss_time': allowed_control_loss_time},
+                                    }
+        control_plane_checker = PingChecker(self.topology_obj.players, validation_control_plane)
+        logger.info('Starting background validation for control plane traffic')
+        control_plane_checker.run_background_validation()
+        return control_plane_checker
+
+    def start_data_plane_validation(self, validation_type, allowed_data_loss_time):
+        # Here we will send 1k pps - it allow to check traffic loss less than 1 second
+        validation_data_plane = {'sender': 'ha',
+                                 'name': 'data_plane_{}'.format(validation_type),
+                                 'background': 'start',
+                                 'args': {'interface': self.ping_sender_iface,
+                                          'count': 200000, 'dst': self.hb_vlan40_ip,
+                                          'interval': 0.001,
+                                          'allowed_traffic_interruptions': 1,
+                                          'allowed_traffic_loss_time': allowed_data_loss_time}}
+        data_plane_checker = PingChecker(self.topology_obj.players, validation_data_plane)
+        logger.info('Starting background validation for data plane traffic')
+        data_plane_checker.run_background_validation()
+        return data_plane_checker
+
+    def do_reboot_or_reload_action(self, action):
+        if 'reload' in action:
+            with allure.step('Reloading the DUT config using cmd: "config reload -y"'):
                 self.cli_object.general.reload_configuration(self.dut_engine)
-
             self.cli_object.general.verify_dockers_are_up(self.dut_engine)
-            self.cli_object.general.check_link_state(self.dut_engine)
-
-            reload_time = time.time() - start_time
-            logger.info('Config reload run time is: {}'.format(reload_time))
-            expected_reload_time = self.expected_reboot_reload_time_dict['config_reload']
-            assert reload_time < expected_reload_time, 'Reload time: {} is bigger ' \
-                                                       'than expected: {}'.format(reload_time, expected_reload_time)
-
-        except Exception as err:
-            raise AssertionError(err)
-        finally:
-            mysql_columns_values = {
-                                    "test_name": request.node.originalname,
-                                    "setup_name": self.setup_name,
-                                    "time_stamp": time.time(),
-                                    "os_version": self.sonic_ver,
-                                    "platform": self.platform,
-                                    "reload_time": reload_time
-            }
-            logger.info('Uploading test results to MySQL DB')
-            DB().insert(table='reload_time', columns_values=mysql_columns_values)
+            self.cli_object.general.check_link_state(self.dut_engine, ifaces=self.topology_obj.players_all_ports['dut'])
+        else:
+            with allure.step('Rebooting the DUT using reboot cmd: "sudo {}"'.format(action)):
+                self.cli_object.general.reboot_flow(self.dut_engine, reboot_type=action, topology_obj=self.topology_obj,
+                                                    wait_after_ping=0)
